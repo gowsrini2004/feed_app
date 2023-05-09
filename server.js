742497a4156cc736391f2023dd301a0432acd935
Gowtham S <gowsrini2004@gmail.com>
2023-05-10 00:11:26 +0530

    page created style added

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
+
+dotenv.config({path:'config.env'})
+const PORT = process.env.PORT||8080
+//log request
+app.use(morgan('tiny'));
+//parser request to body-parser
+app.use(bodyparser.urlencoded({extended:true}))
+//set vie engine
+app.set("view engine","ejs")
+//app.set("views",path.resolve(__dirname,"views/ejs"))
+//load assets
+app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
+app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
+app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+app.listen(3000,()=>{console.log('Server is running on http://localhost:'+PORT)});
